bbf351f2dc5bdeed245ca779950e5cc310801cb0
alumno <alumno@utnfra.com.ar>
2020-02-03 22:26:10 -0300

    ejercicio 2 y3

--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -1,9 +1,16 @@
-/* 
-	Debemos lograr tomar un dato por 'PROMPT' 
+/*
+	Debemos lograr tomar un dato por 'PROMPT'
 	y lo muestro por 'getElementById' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
+
+  var nombre;
+
+  nombre = prompt ("ingrese su nombre")
+
+  document.getElementById("elNombre").nodeValue = nombre
+
 
 }
 
